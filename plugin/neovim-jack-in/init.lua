9c452beef3167fce15911ed3c48f9077a1e3e00a
Trey Bastian <2991824+TreyBastian@users.noreply.github.com>
2024-04-30 23:12:30 +0100

    v0.0.1 neovim-lazy works! refactor and move into it's own plugin tomorrow

--- plugin/neovim-jack-in/init.lua
+++ plugin/neovim-jack-in/init.lua
@@ -1,74 +1,88 @@
 local options = {
-  middlewares = {
-    { dependency = "cider/cider-nrepl", version = "RELEASE" },
+  -- clj dependencies
+  clj_dependencies = {
+    { name = "nrepl/nrepl",       version = "RELEASE" },
+    { name = "cider/cider-nrepl", version = "RELEASE" },
+  },
+  -- clj middleware
+  clj_middleware = {
+    "cider.nrepl/cider-middleware"
+  },
+  -- leiningen plugins
+  lein_plugins = {
+    { name = "cider/cider-nrepl", version = "RELEASE" },
   },
   -- buffer, background, vsplit, split, tab
-  location = 'buffer',
-  win_use_powershell = true,
-  nrepl_version = 'RELEASE',
+  location = 'background',
 }
 
-local is_windows = vim.fn.has('win64')
+local is_powershell = vim.o.shell == 'powershell.exe'
 
--- windows needs to add quotes for powershell because well windows
+-- windows needs to add quotes for powershell because well powershell...
 local function version_escape(version)
-  if is_windows == 1 and options.win_use_powershell == true then
-    return '"""' .. version .. '"""'
+  if is_powershell == true then
+    return '""' .. version .. '""'
   end
   return '"' .. version .. '"'
 end
 
-local function clj_string()
-  -- local string = "clj -Sdeps"
-  -- string = string .. vim.fn.shellescape('\'{:deps {nrepl/nrepl {:mvn/version "RELEASE""} ')
-  -- for _, v in pairs(options.middlewares) do
-  --   string = string .. vim.fn.shellescape(v.dependency .. '{:mvn/version "}' .. v.version .. '""} ')
-  -- end
-  -- string = string .. vim.fn.shellescape('}}\' ')
-  -- string = string .. "--main nrepl.cmdline "
-  -- string = string .. "--middleware = '[\"cider.nrepl/cider-middleware\"]'"
-  -- string = string .. " --interactive"
-  --
-  local string =
-      "clj -Sdeps '{:deps {nrepl/nrepl {:mvn/version " ..
-      version_escape("1.0.0") ..
-      "} cider/cider-nrepl {:mvn/version" ..
-      version_escape("RELEASE") ..
-      "}}}' --main nrepl.cmdline --middleware '[\"cider.nrepl/cider-middleware\"]' --interactive"
+local function map_clj_deps_to_string()
+  local string = ''
+  for _, v in pairs(options.clj_dependencies) do
+    string = string .. v.name .. ' {:mvn/version ' .. version_escape(v.version) .. '} '
+  end
   return string
 end
 
-local execution_functions = {
-  ["buffer"] = "term ",
-  ["background"] = "term ",
-  ["vsplit"] = "vsplit term://",
-  ["split"] = ":split term://",
-  ["tab"] = "tabnew term://",
-}
-
-local function execute(execution_string)
-  local start_string = execution_functions[options.location]
-
-  if options.win_use_powershell == true and is_windows == 1 then
-    start_string = start_string .. 'powershell '
+local function map_clj_middleware_to_string()
+  local string = ''
+  for _, v in pairs(options.clj_middleware) do
+    string = string .. '"' .. v .. '" '
   end
-
-  print(start_string .. execution_string)
-  vim.cmd(start_string .. execution_string)
-  if options.location == 'background' then
-    -- swap to the previous buffer if available
-    vim.cmd(':bp')
-  end
+  return string
 end
 
+local clj_string = "clj -Sdeps " ..
+    "'{:deps {" ..
+    map_clj_deps_to_string() ..
+    "}}' " .. "-M -m nrepl.cmdline --middleware '[" .. map_clj_middleware_to_string() .. "]' --interactive"
 
-function StartClj()
-  execute(clj_string())
+local function map_lein_plugins_to_string()
+  local string = ''
+  for _, v in pairs(options.lein_plugins) do
+    string = string .. v.name .. ' ' .. version_escape(v.version) .. ' '
+  end
+  return string
+end
+
+local lein_string = "lein update-in :plugins conj '[" .. map_lein_plugins_to_string() .. "]' -- repl"
+
+local function jack_in(execution_string)
+  if options.location == "vsplit" then
+    vim.cmd('vsplit')
+  elseif options.location == "split" then
+    vim.cmd('split')
+  elseif options.location == "tab" then
+    vim.cmd('tabnew')
+  end
+
+  vim.cmd(':term ' .. execution_string)
+  if options.location == 'background' then
+    -- swap to the previous buffer if available
+    vim.cmd('bp')
+  end
 end
 
 vim.api.nvim_create_user_command(
   'Clj', function()
-    StartClj()
+    jack_in(clj_string)
+  end,
+  {}
+)
+
+vim.api.nvim_create_user_command(
+  'Lein', function()
+    jack_in(lein_string)
   end,
   {}
 )
